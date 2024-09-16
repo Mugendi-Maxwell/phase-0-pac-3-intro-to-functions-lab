e6a8660cee6882623672ad818b210dc6491c26a6
Maxi-arch <mxmu025@gmail.com>
2024-09-16 23:07:01 +0300

    wow

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function  whisper(string) {
+    return string.toLowerCase()
+    
+}
+ function logShout(string) {
+    const spy ='HELLO'
+    console.log('HELLO')
+    return string.toUpperCase()
+    
+ }
+ function logWhisper(string) {
+    const spy ='hello'
+    console.log('hello')
+    return string.toLowerCase()
+ }
+ function sayHiToHeadphonedRoommate(string) {
+  if (string ==='hello') {
+    return('I can\'t hear you!')
+    
+  } else if (string ==='HELLO'){
+    return('YES INDEED!')
+  }else if (string === "Let\'s have dinner together!")
+    return("I would love to!")
+    
+  }
+    
+ 
